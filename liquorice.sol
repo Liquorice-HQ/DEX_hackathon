03cc14dc4e8139be686a76e4676edeabc4e1065d
Gelato-B2B <105652074+Gelato-B2B@users.noreply.github.com>
2023-03-14 16:56:54 +0400

    Update liquorice.sol

--- liquorice.sol
+++ liquorice.sol
@@ -11,9 +11,12 @@ contract liquorice {
     // event for EVM logging
     event OwnerSet(address indexed oldOwner, address indexed newOwner);
 
+    uint defaultFee; // fee that maker pais for canceling orders
+    uint defaultLockout; // lockout time which is stored in auction when orders are matched
+
     struct order {
         address sender; // address that placed an order
-        int volume; // order volume in ETH
+        uint volume; // order volume in ETH
         bool side; // 0 is BUY, 1 is SELL
         int markup; // positive means maker order, negative means taker order. Range 0 to 100 
     }
@@ -23,6 +26,8 @@ contract liquorice {
         int volume; // order volume in ETH
         bool side; // 0 is BUY, 1 is SELL
         int markup; // positive means maker order, negative means taker order. Range 0 to 100 
+        uint takerfee; // reserved fee to be paid to maker when order is matched 
+        uint makerreserve; // fee paid by maker if he cacnels an order. Should be a small amount 
         uint price; // oracle prices derived at the moment orders were matched
         uint lockout; // timeperiod when cancelation is possible
     }
@@ -35,6 +40,8 @@ contract liquorice {
         console.log("Owner contract deployed by:", msg.sender);
         owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
         emit OwnerSet(address(0), owner);
+        defaultFee = 0.0001 ether;
+        defaultLockout = 3; 
     }
 
     function orderplace(int _volume, bool _side, int _markup) public {
