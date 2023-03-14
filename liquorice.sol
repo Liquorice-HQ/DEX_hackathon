a5308865499feff293c709533ec8383b2b077d3b
Gelato-B2B <105652074+Gelato-B2B@users.noreply.github.com>
2023-03-14 13:00:38 +0400

    Add files via upload

---
+++ liquorice.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract liquorice {
+
+    address private owner;
+
+    // event for EVM logging
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
+
+    struct order {
+        address sender; // address that placed an order
+        int volume; // order volume in ETH
+        bool side; // 0 is BUY, 1 is SELL
+        int markup; // positive means maker order, negative means taker order. Range 0 to 100 
+    }
+
+    mapping(uint => order) public auctions;
+
+    order[] public orders;
+
+    constructor() {
+        console.log("Owner contract deployed by:", msg.sender);
+        owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+        emit OwnerSet(address(0), owner);
+    }
+
+    function orderplace(int _volume, bool _side, int _markup) public {
+        
+    }
+
+}
