67d50b6f6275c60296d890b3148612b540a00312
Gelato-B2B <105652074+Gelato-B2B@users.noreply.github.com>
2023-03-19 00:34:47 +0400

    Update liquorice.sol

--- liquorice.sol
+++ liquorice.sol
@@ -27,19 +27,20 @@ contract liquorice {
     }
 
     struct auction {
+        uint id; //ID of the order
         address sender; // address that placed an order
         uint volume; // order volume in ETH
         bool side; // 0 is BUY, 1 is SELL
         bool TakerMaker; // 0 is taker, 1 is maker        
         int markup; // positive means maker order, negative means taker order. Range 0 to 100 
-        uint takerfee; // reserved fee to be paid to maker when order is matched 
-        uint makerreserve; // fee paid by maker if he cacnels an order. Should be a small amount 
         uint price; // oracle prices derived at the moment orders were matched
         uint lockout; // timeperiod when cancelation is possible
     }
 
+    auction[] public tempAuction;
+
     mapping(int => mapping(uint => order)) public orders; //orders are mapped to associated "markup" value and order id. Example, if two makers place orders with markup 20bp, all orders are mapped to key value 20
-    mapping(uint => mapping(uint => auction)) public auctions; //selection of orders in auction is mapped to associated auction ID
+    mapping(uint => auction[]) public auctions; //selection of orders in auction is mapped to associated auction ID
 
 
     address public constant usdtAddress = 0xdAC17F958D2ee523a2206206994597C13D831ec7; //pushing in USDT address. As mvp we allow to only swap UNI against USDT
@@ -68,58 +69,62 @@ contract liquorice {
         if (_TakerMaker == true) {
             orders[_markup][id] = order(msg.sender, _volume, _side, _TakerMaker, _markup);
         } else {
-            matching(id, _volume, _markup, _side);
+            matching(id, msg.sender, _TakerMaker, _markup,  _side, _volume);
         }
     }
 
     //Does initial calculations to define what happens to taker order
-    function precheck(uint _id, int markup, bool _side, uint _volume) internal view returns(uint checksum, uint[] memory matchedids, uint lastid) {
+    function precheck(uint _id, address _sender, bool _takerMaker, int _markup, bool _side, uint _volume) internal returns(uint checksum) {
         uint sum = 0; //variable used to check if taker found enough maker volume
         uint lastMatchedID; //needed to find last matched id so that its volume can be reduced instead of being carried to auctions fully
         uint[] memory matchedIds;
-        auction[] memory auctionInsert;
+        uint _price = 1500;
+        auction memory tempStruct = auction(_id, _sender, _volume, _side, _takerMaker, _markup, _price, defaultLockout);
+        tempAuction.push(tempStruct);
         if (_side = true) {
-            for (int i = 1; i <= markup; i++) {
+            for (int i = 1; i <= _markup; i++) {
                 for (uint k = 0; k <= _id; k++) {
                     if (sum <= _volume) {
                         sum += orders[i][k].volume;
-                        matchedIds[k] = orders[i][k].id;
+                        matchedIds[k] = k;
+                        auction memory tempStruct = auction(k, orders[i][k].sender, orders[i][k].volume, orders[i][k].side, orders[i][k].TakerMaker, orders[i][k].markup, _price, defaultLockout);
+                        tempAuction.push(tempStruct);
                         break;
                     }
                 }
             }
             
         } else {
-            for (int i = 1; i >= -markup; i--) {
+            for (int i = 1; i >= -_markup; i--) {
                 for (uint k = 0; k <= _id; k++) {
                     if (sum <= _volume) {
                         sum += orders[i][k].volume;
-                        matchedIds[k] = orders[i][k].id;
+                        matchedIds[k] = k;
+                        auction memory tempStruct = auction(k, orders[i][k].sender, orders[i][k].volume, orders[i][k].side, orders[i][k].TakerMaker, orders[i][k].markup, _price, defaultLockout);
+                        tempAuction.push(tempStruct);                        
                         break;
                     }
                 }   
             }
         lastMatchedID = matchedIds[matchedIds.length];
-        delete matchedIds[matchedIds.length];
-        matchedIds[matchedIds.length+1] = _id;
-        return (sum, matchedIds, lastMatchedID);
+        return (sum);
         }
     }
 
     //Matching function. Transfers orders from 
-    function matching(uint _takerID, uint _volume, int markup, bool _side) internal {
+    function matching(uint _id, address _sender, bool _takerMaker, int _markup, bool _side, uint _volume) internal {
         uint _volumecheck;
-        uint[] memory _matchedIds;
-        uint _lastid;
-        (_volumecheck, _matchedIds, _lastid) = precheck(_takerID, markup, _side, _volume);
-        require(_volume <= _volumecheck, "Not enough matching volume");    
-        for (int i = -200; i <= markup; i++) {
-            for (uint k = 0; k <= orders[i].length; k++) {
-                if (orders[i][k].id == _takerID){
-
-                }    
-            }
-        }
+        (_volumecheck) = precheck(_id, _sender, _takerMaker, _markup, _side, _volume);
+        require(_volume <= _volumecheck, "Not enough matching volume");   
+        auctionID++;
+        auctions[auctionID] = tempAuction;
+        
+        for (uint i = 0; i <= tempAuction.length; i++) {
+            _id = tempAuction[i].id;
+            _markup = tempAuction[i].markup;
+            delete orders[_markup][_id];
+        }   
+        delete tempAuction;
     }
 
 
@@ -130,9 +135,9 @@ contract liquorice {
     }
 
     //Called by maker to remove order from auction book
-    function auctioncancel(uint _auctionID, uint _id) external {
-        require(auctions[_auctionID][_id].lockout > block.timestamp, "Lockout period passed");
-        delete auctions[_auctionID][_id];
+    function auctioncancel(uint _auctionID) external {
+        require(auctions[_auctionID][0].lockout > block.timestamp, "Lockout period passed");
+        delete auctions[_auctionID];
     }
 
     //Ideally this function needs to be activated automatically. But in first iteration we can use make it as a manual activation by auction participants
