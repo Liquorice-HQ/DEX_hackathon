3e240da37009c7b612a3eee27ef40f2f15e03b96
ABDul Rehman <devabdee@gmail.com>
2023-03-19 07:04:03 +0500

    project init

---
+++ script/Liquorice.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+
+// import "forge-std/Script.sol";
+
+// contract LiquoriceScript is Script {
+//     function setUp() public {}
+
+//     function run() public {
+//         vm.broadcast();
+//     }
+// }
---
+++ test/Liquorice.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+
+import "forge-std/Test.sol";
+import "../src/Liquorice.sol";
+
+contract LiquoriceTest is Test {
+
+    function setUp() public {
+    }
+
+    function testLiquorice() public {
+
+    }
+}
